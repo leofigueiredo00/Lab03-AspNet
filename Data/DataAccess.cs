25a97c93c824dfd45e03fcead699019778ec70ae
Leonardo Figueiredo <leofigueiredo00@gmail.com>
2024-07-17 11:26:38 -0400

    Initial commit

---
+++ Data/DataAccess.cs
@@ -0,0 +1,47 @@
+﻿namespace Lab03.Data.DataAccess;
+using Lab03.Models;
+using Npgsql;
+using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using Npgsql;
+using Microsoft.Extensions.Configuration;
+
+    public class DataAccess
+{
+    private readonly string _connectionString;
+    public DataAccess(IConfiguration configuration)
+    {
+        _connectionString =
+        configuration.GetConnectionString("DefaultConnection");
+    }
+    public List<YourModel> GetData()
+    {
+        List<YourModel> dataList = new List<YourModel>();
+        using (NpgsqlConnection conn = new NpgsqlConnection(_connectionString))
+        {
+            conn.Open();
+            using (NpgsqlCommand cmd = new NpgsqlCommand("SELECT pid, productname FROM prices", conn))
+            {
+                using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        YourModel data = new YourModel
+                        {
+                            // Assuming YourModel has properties that match your table columns
+                            pid = reader.GetInt32(reader.GetOrdinal("pid")),
+                            productname =
+reader.GetString(reader.GetOrdinal("productname"))
+                            // Add other properties here
+                        };
+                        dataList.Add(data);
+                    }
+                }
+            }
+        }
+        return dataList;
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+using Lab03.Data.DataAccess;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddSingleton<DataAccess>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
